3b782b474306504842c919739a100289f51494f1
lichv <lichvg@gmail.com>
2017-02-08 20:38:42 +0800

    facade

--- src/Socialite.php
+++ src/Socialite.php
@@ -18,4 +18,15 @@ class Socialite extends Facade
     {
         return 'Lichv\\Socialite\\SocialiteManager';
     }
+
+    public static function __callStatic($name, $args)
+    {
+        $app = static::getFacadeRoot();
+
+        if (method_exists($app, $name)) {
+            return call_user_func_array([$app, $name], $args);
+        }
+
+        return $app->$name;
+    }
 }
